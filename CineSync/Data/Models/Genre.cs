dd7f95c1aeb55871159a8b6c356b548984669efe
KingRichardpt45 <ricfrancvi@gmail.com>
2024-04-21 02:47:20 +0100

    feat : Genre

---
+++ CineSync/Data/Models/Genre.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace CineSync.Data.Models
+{
+	public class Genre
+	{
+		public int Id { get; set; }
+
+		public int? TmdbId { get; set; }
+
+		[Required]
+		public string? Name { get; set; }
+	}
+}
