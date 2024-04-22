808773de11471746878662106bad59df501a4a79
KingRichardpt45 <ricfrancvi@gmail.com>
2024-04-22 19:06:42 +0100

    feat NavBAr

---
+++ CineSync/Components/Navs/NavSearch.razor.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.AspNetCore.Components;
+using Microsoft.AspNetCore.Components.Web;
+
+namespace CineSync.Components.Navs
+{
+    public partial class NavSearch
+    {
+        private string _searchActive = string.Empty;
+
+        private string _currentSearch = string.Empty;
+
+        public delegate void Searchhandler(string search);
+        public delegate void openMenu();
+
+        public event Searchhandler? OnSearch;
+
+        private void OutSearch()
+        {
+            _searchActive = "";
+        }
+
+        private void OnSearchClick(MouseEventArgs e)
+        {
+            if (_currentSearch != string.Empty)
+                OnSearch?.Invoke(_currentSearch);
+        }
+
+        private void ClickInput(MouseEventArgs e)
+        {
+            _searchActive = "Active";
+        }
+    }
+}
