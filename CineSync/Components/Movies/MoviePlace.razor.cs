6d33794d7d5a19c0be2141f1444b51769cb61e05
KingRichardpt45 <ricfrancvi@gmail.com>
2024-04-23 11:05:18 +0100

    firx mege conflit in db

---
+++ CineSync/Components/Movies/MoviePlace.razor.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.AspNetCore.Components;
+using CineSync.Data.Models;
+
+namespace CineSync.Components.Movies
+{
+    public partial class MoviePlace
+    {
+
+        private string MoviePosterBase64 = string.Empty;
+
+        [Parameter]
+        public Movie Movie { get; set; }
+
+        [Parameter]
+        public bool UseRatingCs { get; set; } = false;
+
+        [Parameter]
+        public bool UseIMDBRating { get; set; } = false;
+
+        [Parameter]
+        public bool UseTitle { get; set; } = false;
+
+        protected override void OnInitialized()
+        {
+            if(Movie.PosterImage != null)
+                MoviePosterBase64 = Convert.ToBase64String(Movie.PosterImage);
+        }
+    }
+}
