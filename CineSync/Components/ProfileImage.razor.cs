a9b7d7ed76ca801288d1ed1dba630484d0ac4d8c
[ComaN10] <[carlosmanuel2003@gmail.com]>
2024-04-23 12:02:12 +0100

    Resolved Conflicts 7

---
+++ CineSync/Components/ProfileImage.razor.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Components;
+
+public partial class ProfileImage : ComponentBase
+{
+    [Parameter]
+    public EventCallback OnProfileImageChanged { get; set; }
+
+    private async Task SaveProfileImage()
+    {
+        // Implement your logic to save the profile image here
+
+        // Raise event to notify parent component about profile image change
+        await OnProfileImageChanged.InvokeAsync();
+    }
+}
+
