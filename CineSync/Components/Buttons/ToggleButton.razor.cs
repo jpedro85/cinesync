f57599ca325ab8fee609b13cd4248208596757be
Pedro Ferreira <pedroferreira0620@gmail.com>
2024-04-25 15:47:28 +0100

    fix: conflict

---
+++ CineSync/Components/Buttons/ToggleButton.razor.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Components;
+using Microsoft.AspNetCore.Components.Web;
+
+namespace CineSync.Components.Buttons
+{
+    public partial class ToggleButton
+    {
+        [Parameter]
+        public string Title { get; set; } = "Button";
+        public bool state {  get; set; } 
+        public delegate void ButtonAction();
+        public event ButtonAction? OnChange;
+
+        private void OnMouseClick(MouseEventArgs e)
+        {
+            OnChange?.Invoke();
+        }
+    }
+}
