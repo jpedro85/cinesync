240c6494e3d2670a460bbd094ca87f43a1edfbfb
[ComaN10] <[carlosmanuel2003@gmail.com]>
2024-05-02 16:05:48 +0100

    Update:Username Change implemented for the popUP

--- CineSync/Components/Pages/Profile.razor.cs
+++ CineSync/Components/Pages/Profile.razor.cs
@@ -1,11 +1,12 @@
-﻿using CineSync.Data;
+﻿using CineSync.Components.PopUps;
 using Microsoft.AspNetCore.Components;
 
 namespace CineSync.Components.Pages
 {
     public partial class Profile
+    
     {
-        [Parameter]
-        public ApplicationUser? User { get; set; }
+        
+        public  UsernameEdit newuserName {  get; set; }
     }
 }
--- CineSync/Components/PopUps/UsernameEdit.razor.cs
+++ CineSync/Components/PopUps/UsernameEdit.razor.cs
@@ -4,7 +4,12 @@ namespace CineSync.Components.PopUps
 {
     public partial class UsernameEdit
     {
-     
+        public string? newUserName = "";
+
+        public async Task SearchAsync()
+        {
+            await Task.Delay(1000);
+        }
 
     }
 }
