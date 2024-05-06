fc92d65c6f097c3466638694e5d94a3ba0011b57
[ComaN10] <[carlosmanuel2003@gmail.com]>
2024-05-06 16:33:35 +0100

    feat: Initial comments commit

---
+++ CineSync/Components/Comments/Comments.razor.cs
@@ -0,0 +1,18 @@
+﻿namespace CineSync.Components.Comments
+{
+    public partial class Comments
+    {
+        public int Id { get; set; }
+        public int PostId { get; set; }
+        public string UserId { get; set; }
+        public string CommentText { get; set; }
+        public DateTime Created { get; set; }
+
+        public Comments commentModel = new Comments();
+
+        public async Task addComment()
+        {
+
+        }
+    }
+}
