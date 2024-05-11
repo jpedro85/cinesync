0669e01895320e0caf71d48bb1a09c555ac5622a
[ComaN10] <[carlosmanuel2003@gmail.com]>
2024-05-11 19:08:39 +0100

    fix: Comments fix

--- CineSync/Components/Comments/Comments.razor.cs
+++ CineSync/Components/Comments/Comments.razor.cs
@@ -4,20 +4,20 @@ using Microsoft.AspNetCore.Components.Forms;
 using System;
 using System.ComponentModel.DataAnnotations;
 using System.Threading.Tasks;
+using Microsoft.AspNetCore.Components;
 
 namespace CineSync.Components.Comments
 {
     public partial class Commentss
     {
         public uint Id { get; set; }
-        [Required]
+        [Parameter]
         public ApplicationUser? Autor { get; set; }
 
         public long NumberOfLikes { get; set; } = 0;
 
         public long NumberOfDeslikes { get; set; } = 0;
 
-        [Required]
         public string? Content { get; set; }
 
         public ICollection<CommentAttachment>? Attachements { get; set; }
