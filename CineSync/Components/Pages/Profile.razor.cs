65988a44b735dee3dd2b65f34b420411fddf5eb6
[ComaN10] <[carlosmanuel2003@gmail.com]>
2024-05-12 18:41:32 +0100

    Feat: Showing the user Followers count

--- CineSync/Components/Pages/Profile.razor.cs
+++ CineSync/Components/Pages/Profile.razor.cs
@@ -1,5 +1,6 @@
 ﻿using CineSync.Components.PopUps;
 using Microsoft.AspNetCore.Components;
+using CineSync.Data;
 
 namespace CineSync.Components.Pages
 {
@@ -8,5 +9,7 @@ namespace CineSync.Components.Pages
     {
         
         public  UsernameEdit newuserName {  get; set; }
+
+        public ApplicationUser User { get; set; }
     }
 }
