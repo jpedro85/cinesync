a95a0cfdd4068728a03874b5e0270cd5c7c66b5e
KingRichardpt45 <ricfrancvi@gmail.com>
2024-06-04 22:31:49 +0100

    fix merge comflit

---
+++ CineSync/Components/Comments/ItemComment.razor.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNetCore.Components;
+using CineSync.Data.Models;
+using CineSync.DbManagers;
+
+namespace CineSync.Components.Comments
+{
+    public partial class ItemComment
+    {
+        [Inject]
+        private CommentManager CommentManager { get; set; }
+
+        [Parameter]
+        public Comment Comment { get; set; }
+
+        private async void AddLike(Comment commentAddLike)
+        {
+            await CommentManager.AddLikeAsync(commentAddLike);
+            StateHasChanged();
+        }
+
+        private async void AddDeslike(Comment commentAddDesLike)
+        {
+            await CommentManager.AddDesLikeAsync(commentAddDesLike);
+            StateHasChanged();
+        }
+
+    }
+}
