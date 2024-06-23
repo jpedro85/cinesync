f8c92193fd320cc33832cce4aa1ff1a8672d1aed
KingRichardpt45 <ricfrancvi@gmail.com>
2024-06-23 19:58:03 +0100

    fix: loading being cut

---
+++ CineSync/Components/Layout/Loading.razor.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.AspNetCore.Components;
+using System.Drawing;
+
+namespace CineSync.Components.Layout
+{
+	public partial class Loading : ComponentBase
+	{
+		[Parameter]
+		public int FontSize { get; set; } = 80;
+
+		[Parameter]
+		public int LineHeight { get; set; } = 60;
+
+		[Parameter]
+		public int LetterSpacing { get; set; } = 8;
+
+		[Parameter]
+		public int MarginLeterLine { get; set; } = 25;
+
+		[Parameter]
+		public int FontPalceHeight { get; set; } = 40;
+
+		[Parameter]
+		public int MaxWidth { get; set; } = 480;
+		public string _style { get; set; } = string.Empty;
+
+		protected override void OnInitialized()
+		{
+			_style = $"margin-bottom:{MarginLeterLine}px;letter-spacing:{LetterSpacing}px;line-height:{LineHeight}px;font-size:{FontSize}px;";
+		}
+	}
+}
