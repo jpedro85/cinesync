b0b5f6f1b8ecffbc7ceab78eb0068c26ff66283c
KingRichardpt45 <ricfrancvi@gmail.com>
2024-06-29 01:46:18 +0100

    feat ( Item Conversation )

---
+++ CineSync/Components/Converssations/ItemConverssationShort.razor.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.AspNetCore.Components;
+using CineSync.Data.Models;
+using CineSync.DbManagers;
+using CineSync.Data;
+
+namespace CineSync.Components.Converssations
+{
+    public partial class ItemConverssationShort
+    {
+        [Parameter, EditorRequired]
+        public ApplicationUser AuthenticatedUser { get; set; } = default!;
+
+        [Parameter,EditorRequired]
+        public Conversation Conversation { get; set; } = default!;
+
+		[Parameter]
+		public EventCallback OnClick { get; set; } = default!;
+
+		[Parameter]
+		public int MaxGroupParticipantsToShow { get; set; } = 5;
+
+
+		[Inject]
+        public ConverssationManager ConverssationManager { get; set; } = default!;
+
+		private bool _hasNewMessage = true;
+
+		//TODO: UNCOMMENT
+		//protected override async Task OnInitializedAsync()
+		//{
+		//    Conversation.Participants = await ConverssationManager.GetParticipants(Conversation);
+		//}
+
+		public void UpdateNotificationState( bool newState) 
+		{
+			_hasNewMessage = newState;
+			InvokeAsync(StateHasChanged);
+		}
+	}
+}
