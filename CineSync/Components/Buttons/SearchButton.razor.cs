bac871a8098b8d591c786ec011ca116aa8da2629
KingRichardpt45 <ricfrancvi@gmail.com>
2024-06-30 02:48:37 +0100

    feat ( PopUp Search User)

--- CineSync/Components/Buttons/SearchButton.razor.cs
+++ CineSync/Components/Buttons/SearchButton.razor.cs
@@ -15,21 +15,65 @@ namespace CineSync.Components.Buttons
         [Parameter]
         public string SearchInput { get; set; } = string.Empty;
 
-        public delegate void SearchResultHandler( string searchResults );
-        public event SearchResultHandler? OnSearch;
+		[Parameter]
+		public bool AllowNotEmpty { get; set; } = false;
+
+		[Parameter]
+        public string PlaceHolder { get; set; } = "Search";
+
+        [Parameter]
+		public int Heigth { get; set; } = 50;
+
+		[Parameter]
+		public int InputWidth { get; set; } = 200;
+
+        [Parameter]
+        public string Width { get; set; } = "0";
+
+		[Parameter]
+		public int SearchButtonWidth { get; set; } =  70;
+
+		[Parameter]
+		public int SearchButtonIconSize{ get; set; } = 20;
+
+        [Parameter]
+        public int PadingBorder { get; set; } = 20;
+
+        [Parameter]
+        public bool Animation { get; set; } = true;
+
+		[Parameter]
+        public EventCallback<string> OnSearch { get; set; }
+
+        [Parameter]
+        public bool ShowButton {  get; set; } = true;
+
+        public delegate void Onkeydown(string search);
+
+        [Parameter]
+        public Onkeydown OnInput { get; set; } = (e) => { };
+
+        //public delegate void searchresulthandler( string searchresults );
+        //public event searchresulthandler? onsearch;
+
+        private void OnInputChange(ChangeEventArgs e)
+        {
+            SearchInput = e.Value.ToString();
+            OnInput(SearchInput ?? "");
+        }
 
         private void SearchHandler( KeyboardEventArgs  e )
         {
-            if ( e.Key == "Enter" && !string.IsNullOrEmpty(SearchInput) )
+            if ( OnSearch.HasDelegate && e.Key == "Enter" && ( AllowNotEmpty || !string.IsNullOrEmpty(SearchInput) ) )
             {
-                OnSearch?.Invoke(SearchInput);
+                OnSearch.InvokeAsync(SearchInput);
             }
         }
 
         private void SearchHandler( MouseEventArgs e )
         {
-            if( !string.IsNullOrEmpty(SearchInput) )
-                OnSearch?.Invoke(SearchInput);
+            if(OnSearch.HasDelegate && !string.IsNullOrEmpty(SearchInput) )
+                OnSearch.InvokeAsync(SearchInput);
         }
 
 	}
---
+++ CineSync/Components/PopUps/PopUpSearchUser.razor.cs
@@ -0,0 +1,130 @@
+﻿using CineSync.Data;
+using CineSync.DbManagers;
+using Microsoft.AspNetCore.Components;
+using Microsoft.AspNetCore.Components.Web;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.IdentityModel.Tokens;
+using System.Collections;
+
+namespace CineSync.Components.PopUps
+{
+    public partial class PopUpSearchUser : ComponentBase
+    {
+        [Parameter, EditorRequired]
+        public ApplicationUser AuthenticatedUser { get; set; } = default!;
+
+		[Parameter, EditorRequired]
+        public string Id { get; set; } = string.Empty; 
+        private string _id = "PopUpSearchUser_";
+
+        [Parameter]
+        public string Title { get; set; } = "Search User" ;
+
+		[Parameter]
+		public EventCallback<ApplicationUser> OnClickUser { get; set; }
+
+        [Inject]
+        public UserManager DbUserManager { get; set; } = default!;
+
+		[Inject]
+		private ILookupNormalizer KeyNormalizer { get; set; } = default!;
+
+		private PopUpLayout _popupLayout = default!;
+
+		private string _searchContent = string.Empty;
+		private bool _isLoading = false;
+		private bool _MoreResultsLoading = false;
+		private IEnumerable<ApplicationUser> _followingResults = [];
+		private IEnumerable<ApplicationUser> _DbResults = [];
+        public ICollection<ApplicationUser> _Following { get; set; } = [];
+
+
+        protected override void OnInitialized()
+        {
+            _id = "PopUpSearchUser_" + Id;
+			_Following = AuthenticatedUser.Following ?? []; 
+
+		}
+
+		public void Open() 
+		{
+			_popupLayout.Open();
+		}
+
+		public void Close() 
+		{
+			_popupLayout.Close();
+		}
+
+        private void OnClickSearch( string searchUsername ) 
+        {
+			_searchContent = searchUsername;
+			_DbResults = [];
+			_followingResults = [];
+
+			if (searchUsername.IsNullOrEmpty()) 
+				return;
+
+			searchUsername = KeyNormalizer.NormalizeName( searchUsername.Trim() );
+
+			Task.Run(() => { SearchUsername(searchUsername); });
+
+			_isLoading = true;
+			StateHasChanged();
+        }
+
+		private void SearchUsername(string searchUsername)
+		{
+            _followingResults = _Following.Where(u => u.Id != AuthenticatedUser.Id &&
+													u.NormalizedUserName != null &&
+													u.NormalizedUserName.Contains(searchUsername)
+												);
+			if (_followingResults.Count() == 0)
+			{
+				SearchUsernameInDb(searchUsername);
+			}
+			else
+			{
+				_isLoading = false;
+				 InvokeAsync(StateHasChanged);
+			}
+		}
+
+		private void OnClickMoreResults()
+		{
+			if (_searchContent.IsNullOrEmpty())
+				return;
+
+			var normalizedUsername = KeyNormalizer.NormalizeName( _searchContent.Trim() );
+
+            Task.Run(() => { SearchUsernameInDb(normalizedUsername); });
+
+			_MoreResultsLoading = true;
+			StateHasChanged();
+		}
+
+        private async void SearchUsernameInDb ( string searchUsername ) 
+        {
+            _DbResults = await DbUserManager.GetByConditionAsync( u => u.Id != AuthenticatedUser.Id && 
+																  u.NormalizedUserName != null &&
+							                                      u.NormalizedUserName.Contains(searchUsername)
+						                                        );
+
+			_DbResults = _DbResults.Where( u => !_followingResults.Contains(u) );
+			_isLoading = false;
+			_MoreResultsLoading = false;
+			await InvokeAsync(StateHasChanged);
+		}
+
+		private void OnInput( string search ) 
+		{
+			if (search.IsNullOrEmpty() )
+			{
+				_DbResults = [];
+				_followingResults = [];
+				InvokeAsync(StateHasChanged);
+			}
+		}
+
+    }
+}
